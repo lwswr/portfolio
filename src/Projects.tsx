7bad2b7a6968ec66f57c11f4c8ec73cf53fe67aa
Lewis Wear <lewiswear@Lewiss-MacBook-Pro.local>
2020-11-17 14:11:40 +0000

    replaced ipsum with actual project description

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -14,21 +14,21 @@ export const Projects = () => {
     <ProjectsContainer>
       <ProjectCard
         title={"COVID TRACKER"}
-        description={LoremIpsum}
+        description={covidTrackerDes}
         image={"./images/covid-tracker1.png"}
         githubURL={"https://github.com/lwswr/covid-tracker"}
         demoURL={"https://lwswr.github.io/covid-tracker/"}
       />
       <ProjectCard
         title={"WEATHER APPLIACTION"}
-        description={LoremIpsum}
+        description={weatherAppDes}
         image={"./images/weather-app.png"}
         githubURL={"https://github.com/lwswr/weather-app"}
         demoURL={"https://lwswr.github.io/weather-app/"}
       />
       <ProjectCard
         title={"MOVIE DATABASE"}
-        description={LoremIpsum}
+        description={movieDBDes}
         image={"./images/movie-db1.png"}
         githubURL={"https://github.com/lwswr/movie-db"}
         demoURL={"https://lwswr.github.io/movie-db/"}
@@ -37,5 +37,11 @@ export const Projects = () => {
   );
 };
 
-const LoremIpsum: string =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const covidTrackerDes: string =
+  "Single page application showing COVID-19 data for countries and displaying the information with tables and graphs. Technologies used for development: React, Typescript, Axios Promise based http client, covid19api.com API,  styled-components for styling, ChartJS for displaying a graph with a timeline.";
+
+const weatherAppDes: string =
+  "Single page application which allows the user to search different cities around the world. The app displays the data in an intuitive way and gives a 3 day forecast for that location. Technologies used for development: React, Typescript, Axios Promise based http client, OpenweatherAPI, styled-components for styling.";
+
+const movieDBDes: string =
+  "Single page application which displays a list of movies based on user input and allows the user to expand different movie tiles to access more information. React, Typescript, Axios Promise based http client, oMDB API, styled-components for styling.";
